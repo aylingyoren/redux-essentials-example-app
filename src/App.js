4c86708f40673fae50c23b294c3dde9a2e5e970f
aylingyoren <aylingyoren21@gmail.com>
2021-11-11 19:44:51 +0300

    add n edit posts features

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
-} from 'react-router-dom'
-
-import { Navbar } from './app/Navbar'
+} from 'react-router-dom';
+import { PostsList } from './features/posts/PostsList';
+import { AddPostForm } from './features/posts/AddPostForm';
+import { SinglePostPage } from './features/posts/SinglePostPage';
+import { EditPostForm } from './features/posts/EditPostForm';
+import { Navbar } from './app/Navbar';
 
 function App() {
   return (
@@ -18,11 +21,14 @@ function App() {
             exact
             path="/"
             render={() => (
-              <section>
-                <h2>Welcome to the Redux Essentials example app!</h2>
-              </section>
+              <>
+                <AddPostForm />
+                <PostsList />
+              </>
             )}
           />
+          <Route exact path='/posts/:postId' component={SinglePostPage} />
+          <Route exact path='/editPost/:postId' component={EditPostForm} />
           <Redirect to="/" />
         </Switch>
       </div>
